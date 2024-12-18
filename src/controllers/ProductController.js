2e580f4b14cf72fab4cffd9d3a07a043c248fcbb
Nguyễn Hoàng Anh Vũ <dh52112101@student.stu.edu.vn>
2024-12-18 09:12:20 +0700

    API updateProduct

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -20,6 +20,7 @@ const createProduct = async (req, res) => {
         })
     }
 }
+
 const updateProduct = async (req, res) => {
     try {
         const idProduct = req.params.id;
@@ -39,118 +40,7 @@ const updateProduct = async (req, res) => {
         })
     }
 }
-const deleteProduct = async (req, res) => {
-    try {
-        const idProduct = req.params.id;
-        if (!idProduct) {
-            return res.status(404).json({
-                status: 'error',
-                message: "Vui lòng chọn id"
-            })
-        }
-        const response = await ProductService.deleteProduct(idProduct);
-        return res.status(200).json({
-            message: "Ok",
-            data: response
-        })
-    } catch (error) {
-        return res.status(404).json({
-            status: 'error',
-            message: "Lỗi từ controller"
-        })
-    }
-}
-const getDetailProduct = async (req, res) => {
-    try {
-        const idUser = req.params.id;
-        if (!idUser) {
-            return res.status(404).json({
-                status: "Error",
-                message: "Vui lòng nhập id Product"
-            })
-        }
-        const response = await ProductService.getDetailProduct(idUser);
-        return res.status(200).json({
-            status: "Ok",
-            response
-        })
-    } catch (error) {
-        return res.status(404).json({
-            status: "Error",
-            message: "Lỗi phía controller"
-        })
-    }
-}
-const getAllProduct = async (req, res) => {
-    try {
-        const { limit, page, sort, filter } = req.query
-        const response = await ProductService.getAllProduct(Number(limit) || null, Number(page) || 0, sort, filter)
-        return res.status(200).json(response)
-    } catch (e) {
-        return res.status(404).json({
-            message: e
-        })
-    }
-}
-const getAllProduct2 = async (req, res) => {
-    try {
-        const { limit, page, sort, filter } = req.query
 
-        const response = await ProductService.getAllProduct2(Number(limit) || null, Number(page) || 0, sort, filter)
-        return res.status(200).json(response)
-    } catch (e) {
-        return res.status(404).json({
-            status: 'errrr',
-            message: e
-        })
-    }
-}
-
-const deleteManyProduct = async (req, res) => {
-    try {
-        const ids = req.body.ids
-        if (!ids) {
-            return res.status(404).json({
-                status: 'Error',
-                message: 'Vui long chon product'
-            })
-        }
-        const response = await ProductService.deleteManyProduct(ids);
-        return res.status(200).json(response)
-    } catch (error) {
-        return res.status(404).json({
-            status: 'Error',
-            message: 'Loi tu services',
-            error
-        })
-    }
-}
-const getAllTypeProduct = async (req, res) => {
-    try {
-        const response = await ProductService.getAllTypeProduct();
-        return res.status(200).json(response)
-    } catch (error) {
-        return res.status(404).json({
-            message: 'Error from services!!',
-            status: 'err'
-        })
-    }
-}
-const getAllDiscount = async (req, res) => {
-    try {
-        const response = await ProductService.getAllDiscount();
-        return res.status(200).json(response)
-    } catch (error) {
-        return res.status(404).json({
-            message: 'Error from services!!',
-            status: 'err'
-        })
-    }
-}
 module.exports = {
-    createProduct,
-    updateProduct, deleteProduct,
-    getAllProduct, getDetailProduct,
-    getAllProduct2, deleteManyProduct,
-    getAllTypeProduct, getAllDiscount
+    createProduct, updateProduct
 }
