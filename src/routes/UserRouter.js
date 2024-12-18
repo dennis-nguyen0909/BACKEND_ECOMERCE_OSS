92aefa9ad0483576a65f067ea575d5456e3f2231
dennis <dennis.nguyen0909@gmail.com>
2024-12-18 08:32:49 +0700

    create-api-delete-user

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const UserController = require("../controllers/UserController");
-const { authUserMiddleware } = require("../middleware/authMiddleware");
+const {
+  authUserMiddleware,
+  authMiddleware,
+} = require("../middleware/authMiddleware");
 const router = express.Router();
 const UserRouter = (app) => {
   router.post("/sign-up", UserController.createUser);
   router.post("/login", UserController.loginUser);
   router.put("/update-user/:id", authUserMiddleware, UserController.updateUser);
+  router.delete("/delete-user/:id", authMiddleware, UserController.deleteUser);
   return app.use("/api/user", router);
 };
 
