34f4fb619cc12a532b512b8adeaa0e4eec42b5ed
dennis <dennis.nguyen0909@gmail.com>
2024-12-18 08:30:39 +0700

    api-update-user

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const UserController = require("../controllers/UserController");
+const { authUserMiddleware } = require("../middleware/authMiddleware");
 const router = express.Router();
 const UserRouter = (app) => {
   router.post("/sign-up", UserController.createUser);
   router.post("/login", UserController.loginUser);
+  router.put("/update-user/:id", authUserMiddleware, UserController.updateUser);
   return app.use("/api/user", router);
 };
 
