8ee0d142ef10d9e8e960e7386c03ab95d1b86384
Nguyễn Hoàng Anh Vũ <dh52112101@student.stu.edu.vn>
2024-12-18 10:44:52 +0700

    API getAllOrderDetails

--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -145,8 +145,34 @@ const getAllOder = () => {
   });
 };
 
+const getAllOrderDetails = (id) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const order = await Order.find({
+        user: id,
+      }).sort({ createdAt: -1, updatedAt: -1 });
+      if (order === null) {
+        resolve({
+          status: "ERR",
+          message: "The order is not defined",
+        });
+      }
+
+      resolve({
+        status: "OK",
+        message: "SUCESSS",
+        data: order,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
 
 
 module.exports = {
-  getAllOrderDetailsByMonth, createOrder, getAllOder
+  getAllOrderDetailsByMonth,
+   createOrder, getAllOder,
+   getAllOrderDetails
 };
